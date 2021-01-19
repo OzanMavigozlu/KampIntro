f3c025efbcf0a1218011f27187f390e4febc46ac
Ozan Mavigozlu <77669908+OzanMavigozlu@users.noreply.github.com>
2021-01-19 23:03:12 +0200

    Add files via upload

---
+++ RefAndOut/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RefOutMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 50;
+            int sayi2 = 231;
+            Topla(ref sayi1, 12);
+            Topla2(out sayi2, 12);
+
+        }
+
+        static void Topla(ref int sayi1, int sayi2)
+        {
+            //sayi1 = 20;
+            Console.WriteLine(sayi1 + sayi2);
+
+        }
+        static void Topla2(out int sayi1, int sayi2)
+        {
+            sayi1 = 10;
+            Console.WriteLine(sayi1 + sayi2);
+
+        }
+    }
+}
